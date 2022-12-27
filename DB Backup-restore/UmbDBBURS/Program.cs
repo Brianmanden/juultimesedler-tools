261d1c18d7839b5a076eeb7c5d11a81dade8db5b
Brian Andersen <brianmanden@gmail.com>
2022-12-27 20:35:24 +0100

    ADD UI Chose backup from list of directories in folder

--- DB Backup-restore/UmbDBBURS/Program.cs
+++ DB Backup-restore/UmbDBBURS/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 
 using Spectre.Console;
+using System.Collections.Generic;
 using System.Configuration;
 
 namespace UmbDBBURS
@@ -16,11 +17,40 @@ namespace UmbDBBURS
 
             var sourceDir = configuration.GetSection("Source");
             var destDir = configuration.GetSection("Destination");
-            var parameters = args.Length > 0 ? args[0] : "";
+            var parameters = args.Length > 0 ? args[0].ToString() : "";
 
             AnsiConsole.MarkupLine($"[red] Source: [/]" + sourceDir.Value);
             AnsiConsole.MarkupLine($"[yellow] Destination: [/]" + destDir.Value);
-            AnsiConsole.MarkupLine($"[green] Args:  [/]" + parameters);
+
+            IEnumerable<string> directories = Directory.EnumerateDirectories(destDir.Value);
+            string chosenBackup = "";
+
+            if (String.IsNullOrEmpty(parameters))
+            {
+                return 0;
+            }
+
+            switch (parameters)
+            {
+                case "rs":
+                    chosenBackup = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("Chose backup to restore")
+                        .PageSize(10)
+                        .MoreChoicesText("[grey] Move up/down to chose backup [/]")
+                        .AddChoices(directories.ToArray()));
+                    // TODO Restore DB with files
+                    AnsiConsole.MarkupLine("[green] Chosen backup:[/] " + chosenBackup);
+                break;
+
+                case "bu":
+                    // TODO Backup DB files to folder
+                    AnsiConsole.MarkupLine("Backup DB files");
+                break;
+
+                default:
+                    break;
+            }
 
             return 1;
         }
