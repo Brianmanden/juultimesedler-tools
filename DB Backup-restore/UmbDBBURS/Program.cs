803875d7c1088efa2015fa603d3f6a01030cb321
Brian Andersen <brianmanden@gmail.com>
2022-12-27 20:03:13 +0100

    ADD Use appsettings.json ADD Use args from command line

---
+++ DB Backup-restore/UmbDBBURS/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using Spectre.Console;
+using System.Configuration;
+
+namespace UmbDBBURS
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                                    .SetBasePath(Directory.GetCurrentDirectory())
+                                    .AddJsonFile("appsettings.json")
+                                    .Build();
+
+            var sourceDir = configuration.GetSection("Source");
+            var destDir = configuration.GetSection("Destination");
+            var parameters = args.Length > 0 ? args[0] : "";
+
+            AnsiConsole.MarkupLine($"[red] Source: [/]" + sourceDir.Value);
+            AnsiConsole.MarkupLine($"[yellow] Destination: [/]" + destDir.Value);
+            AnsiConsole.MarkupLine($"[green] Args:  [/]" + parameters);
+
+            return 1;
+        }
+    }
+}
